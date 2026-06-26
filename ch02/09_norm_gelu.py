b76d15ef8160af3a0e2168f8a40541669c37b2e7
t-act <takuto.505521@gmail.com>
2026-06-26 19:11:27 +0900

    feat: ch02にGELU・FFN・Transformerブロックを実装

--- ch02/09_norm_gelu.py
+++ ch02/09_norm_gelu.py
@@ -19,3 +19,41 @@ class LayerNorm(nn.Module):
         norm_x = (x - mean) / torch.sqrt(var + self.eps)
         return self.gamma * norm_x + self.beta
     
+
+class GELU(nn.Module):
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) * (x + 0.044715 * torch.pow(x,3))
+        ))
+
+
+class FFN(nn.Module):
+    def __init__(self, x_dim, hidden_dim=None, dropout_rate=0.1):
+        super().__init__()
+        if hidden_dim is None:
+            hidden_dim = int(4 * x_dim)
+        
+        self.layers = nn.Sequential(
+            nn.Linear(x_dim, hidden_dim),
+            GELU(),
+            nn.Linear(hidden_dim, x_dim),
+            nn.Dropout(dropout_rate)
+        )
+    
+    def forward(self, x):
+        return self.layers(x)
+
+
+class Block(nn.Module):
+    def __init__(self, embed_dim, n_head, ff_dim=None, dropout_rate=0.1):
+        super().__init__()
+        head_dim = embed_dim // n_head
+        self.norm1 = LayerNorm(embed_dim)
+        self.attn = MultiHeadAttention(embed_dim, n_head, head_dim, dropout_rate)
+        self.norm2 = LayerNorm(embed_dim)
+        self.ffn = FFN(embed_dim, ff_dim, dropout_rate)
+    
+    def forward(self, x):
+        x = x + self.attn(self.norm1(x))
+        x = x + self.ffn(self.norm2(x))
+        return x
